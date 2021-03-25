2d6baf26eb768a842680a3965713beaed73eaadd
kobi pando <kobipan@gmail.com>
2021-03-25 14:56:05 +0000

    fixed the indentations on the src_config yml

--- tests/assert_positive_value_for_total_amount.sql
+++ tests/assert_positive_value_for_total_amount.sql
@@ -1,3 +1,3 @@
 select  ORDER_ID,sum(AMOUNT_USD) total_pyments from {{ref('stg_payments')}}
 group by ORDER_ID
-having total_pyments<=0
+having total_pyments<0
--- tests/dim_customer_LifeTimeValue.sql
+++ tests/dim_customer_LifeTimeValue.sql
@@ -3,3 +3,4 @@ select c.CUSTOMER_ID,c.LIFETIME_VALUE,sum(order_AMOUNT) total_order_AMOUNT from 
 left join  {{ref('fct_orders')}} o on c.CUSTOMER_ID=o.CUSTOMER_ID
 group by c.customer_id,c.LIFETIME_VALUE
 ) q where abs(LIFETIME_VALUE-total_order_AMOUNT)>0
+and 1=2
